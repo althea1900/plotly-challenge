a27e086953170ec4320450a076e591a8b9892347
Althea McMillian <amcmilli@richmond.edu>
2020-09-11 15:41:10 -0400

    final tweeks

--- static/js/app.js
+++ static/js/app.js
@@ -76,7 +76,7 @@ function buildPlot(id) {
 }
 
 function demoInfo(id) {
-    d3.json("./data/samples.json").then((data) => {
+    d3.json("../data/samples.json").then((data) => {
 
         // Store the metadata 
         var metadata = data.metadata;
@@ -104,7 +104,7 @@ function optionChanged(newSubject) {
 function init() {
     var dropdown = d3.select("#selDataset");
 
-    d3.json("./data/samples.json").then((data) => {
+    d3.json("../data/samples.json").then((data) => {
         console.log(data)
     // });
 
