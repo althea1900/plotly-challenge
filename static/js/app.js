622c2964032fdbc52fa1d08ac04b3f7b0f56d77d
Althea McMillian <amcmilli@richmond.edu>
2020-09-11 11:58:59 -0400

    starting to build dashboard

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,31 @@
-d3.json("./data/samples.json").then(function(data){
-    console.log(data);
-});
+// var dropdown = d3.select("#selDataset");
+
+// d3.json("./data/samples.json").then((data) =>{
+//     console.log(data);
+
+//     // get the selected Human Sample
+//     data.names.forEach(function(name) {
+//         dropdown.append("option").text(name).property("value");
+//     });
+// });
+
+function init() {
+    var dropdown = d3.select("#selDataset");
+
+    d3.json("./data/samples.json").then((data) => {
+        console.log(data)
+    // });
+
+    // get the selected Human Sample
+    data.names.forEach(function(name) {
+        dropdown.append("option").text(name).property("value");
+    });
+
+    });
+}
+
+init();
+// function getHumanSample(data) {
+
+// }
+
